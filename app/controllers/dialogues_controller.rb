8265a325af6554aaf434c1551b5ad4d45930ae83
Cool BRG <budhram.gurung01@gmail.com>
2020-07-03 15:24:48 +0530

    new and create with views

--- app/controllers/dialogues_controller.rb
+++ app/controllers/dialogues_controller.rb
@@ -1,9 +1,29 @@
 class DialoguesController < ApplicationController
   def index
-    @dialgues = Dialogue.all
+    @dialogues = Dialogue.all
   end
 
   def show
     @dialogue = Dialogue.find(params[:id])
+  end
+
+  def new
+    @dialogue = Dialogue.new
+  end
+
+  def create
+    @dialogue = Dialogue.new(dialogue_params)
+    if @dialogue.save
+      flash[:notice] = "Dialogue created successfully."
+      redirect_to @dialogue
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def dialogue_params
+    params.require(:dialogue).permit(:line, :movie, :nonveg)
   end
 end
