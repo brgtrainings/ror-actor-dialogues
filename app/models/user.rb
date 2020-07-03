8cb4ade9a5febcc15846f28deabef37cefe323ed
Cool BRG <budhram.gurung01@gmail.com>
2020-07-03 17:23:05 +0530

    name and age in user sign up and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :dialogues
+
+  validates :name, presence: :true, length: { minimum: 3 }
+  validates :age, numericality: { only_integer: true }
 end
