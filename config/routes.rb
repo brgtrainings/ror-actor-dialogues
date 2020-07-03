43e784837988e3262b185ba419a1e2d687a480ee
Cool BRG <budhram.gurung01@gmail.com>
2020-07-03 16:45:31 +0530

    home page and root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get '/dialogues', to: 'dialogues#index'
   # get '/dialogues/:id', to: 'dialogues#show'
   resources :dialogues, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  root "pages#home"
 end
